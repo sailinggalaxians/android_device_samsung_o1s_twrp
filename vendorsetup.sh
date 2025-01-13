da49c686ade1e8cea96e7f632523903a8175a2e1
Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>
2025-01-13 23:13:57 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,7 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_o1s-user
-add_lunch_combo omni_o1s-userdebug
-add_lunch_combo omni_o1s-eng
+add_lunch_combo twrp_o1s-eng
