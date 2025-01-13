7672201794e95cb92f560157558bc60e2698dfc4
Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>
2025-01-13 23:13:05 +0100

    Update twrp_o1s.mk

--- twrp_o1s.mk
+++ twrp_o1s.mk
@@ -1,29 +1,24 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# Release name
+PRODUCT_RELEASE_NAME := o1s
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Inherit from o1s device
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/samsung/o1s/device.mk)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/o1s/recovery/root,recovery/root)
+
 PRODUCT_DEVICE := o1s
-PRODUCT_NAME := omni_o1s
+PRODUCT_NAME := twrp_o1s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G991B
 PRODUCT_MANUFACTURER := samsung
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="o1sxxx-user 11 RP1A.200720.012 G991BXXSBGXED release-keys"
-
-BUILD_FINGERPRINT := samsung/o1sxxx/o1s:11/RP1A.200720.012/G991BXXSBGXED:user/release-keys
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
